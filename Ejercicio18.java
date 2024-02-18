ff459916c1b8654c4eb270027ae99afb980458b9
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-02-18 14:15:28 -0500

    Ejercicio18 resuelto

---
+++ Ejercicio18.java
@@ -0,0 +1,7 @@
+public class Ejercicio18 {
+    public static void main (String[]args){
+        double numero = 125;
+        double cuadrado = Math.pow(numero, 2);
+        System.out.println("El cuadrado de "+numero+" es "+cuadrado);
+    }
+}
