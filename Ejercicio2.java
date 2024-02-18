a205826f5b0e1389c0a75a097117f36dd1e78ede
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-02-18 00:03:50 -0500

    Ejercicio2 Resuelto

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ejercicio2 {
+    public static void main(String[]args){
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Ingresa un texto");
+        String texto = Entrada.nextLine();
+        System.out.println();
+        System.out.println("El texto ingresado es: ");
+        System.out.println(""+texto);
+    }
+}
