9743722cebea39fb550d2df279a140740667f40d
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-02-20 15:19:41 -0500

    Ejercicio21 resuelto

---
+++ Ejercicio21.java
@@ -0,0 +1,10 @@
+public class Ejercicio21 {
+    public static void main(String[]args){
+        int A = 10;
+        int B = 23;
+        int dato = A;
+        A = B;
+        B = dato;
+        System.out.println(""+A+""+B);
+    }
+}
