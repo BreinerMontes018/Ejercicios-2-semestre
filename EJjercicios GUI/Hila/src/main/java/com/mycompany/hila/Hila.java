42d13276e92bbb8ae881e86ceb225546b6747b03
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-04-21 17:16:11 -0500

    Ejercicio180 resuelto

---
+++ EJjercicios GUI/Hila/src/main/java/com/mycompany/hila/Hila.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hila;
+
+/**
+ *
+ * @author Breiner
+ */
+public class Hila {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
